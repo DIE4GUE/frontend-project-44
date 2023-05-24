f12f855f6933fccf92afdcddd1d44f65b6a28221
DIE4GUE <cacaha1412@gmail.com>
2023-05-24 11:59:40 +0300

    step3

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,6 @@
 #!/usr/bin/env node
-console.log('Welcome to the Brain Games!');
+    console.log('Welcome to the Brain Games!');
+
+    import sayGreeting from '../src/sli.js';
+
+    sayGreeting();
