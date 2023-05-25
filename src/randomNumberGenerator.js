446509238ed81597eb284c191c25b9a61754684e
DIE4GUE <cacaha1412@gmail.com>
2023-05-25 11:22:59 +0300

    step5, first game

---
+++ src/randomNumberGenerator.js
@@ -0,0 +1,6 @@
+const generateRandomNumber = (max = 100, min = 0) => {
+    const randomized = Math.floor(Math.random() * (max - min + 1)) + min;
+    return randomized;
+  };
+  export default generateRandomNumber;
+  
